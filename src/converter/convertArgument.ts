6f19525a095e335877c16c5a86c81f8ecb57733f
aoisupersix <aoisupersix@gmail.com>
2019-07-27 21:57:28 +0900

    [refactor] argument type was changed to use new type definition

--- src/converter/convertArgument.ts
+++ src/converter/convertArgument.ts
@@ -1,6 +1,6 @@
 import { IArgumentDefinition } from '../definition/iArgumentDefinition'
 import { IArgument } from '../definition/iArgument'
-import { argumentTypes } from '../common/argumentTypes'
+import { ArgumentType } from '../common/argumentTypes'
 
 /**
  * IArgumentDefinitionに情報を付与したIArguementを生成して返します。
@@ -28,10 +28,10 @@ const convertArgument = (argDef: IArgumentDefinition): IArgument => {
     argument.last = false
 
     // テスト値の設定
-    const targetType = argumentTypes.find(type => type.isType(argument.type))!
-    argument.test_value_map_grammar = targetType.getOutputValue()
-    argument.test_value_map_grammar_non_quote = targetType.testValue
-    argument.test_value_csharp = targetType.getCSharpTestValue()
+    const targetType = ArgumentType.allTypes.find(type => type.isType(argument.type))!
+    argument.test_value_map_grammar = targetType.bve5TestValue
+    argument.test_value_map_grammar_non_quote = targetType.rowTestValue
+    argument.test_value_csharp = targetType.csharpTestValue
 
     return argument
 }
