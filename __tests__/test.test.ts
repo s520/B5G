0806b2ee59e5e730ffd2aa4e70e49835073347c1
aoisupersix <aoisupersix@gmail.com>
2019-07-27 16:59:07 +0900

    [add] jest test suite

---
+++ __tests__/test.test.ts
@@ -0,0 +1,9 @@
+import * as argType from '../src/common/argumentTypes'
+
+describe('DoubleArgument', () => {
+    describe('isType()', () => {
+        it('same type', () => {
+            expect(1).toBe(1)
+        })
+    })
+})
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/src/**/*.ts",
+    "!**/node_modules/**"
+  ],
+  
+};
