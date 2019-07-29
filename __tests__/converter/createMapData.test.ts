9b89461568decefc3d0fb42917966a9c031488bc
aoisupersix <aoisupersix@gmail.com>
2019-07-29 22:02:26 +0900

    [add] createMapData() called multi statement tests

--- __tests__/converter/createMapData.test.ts
+++ __tests__/converter/createMapData.test.ts
@@ -37,4 +37,30 @@ describe('createMapData()', () => {
         expect(mapData.funcs.length).toBe(1)
         expect(mapData.funcs[0]).toBe('Function')
     })
+
+    it('multi statement', () => {
+        const states = [
+            Object.assign({}, statement),
+            Object.assign({}, statement),
+            Object.assign({}, statement)
+        ]
+        states[0].elem = 'Element1'
+        states[1].elem = 'Element2'
+        states[2].elem = 'Element3'
+        states[0].sub_elem = 'SubElement1'
+        states[1].sub_elem = 'SubElement2'
+        states[2].sub_elem = 'SubElement3'
+        states[0].func = 'Function1'
+        states[1].func = 'Function2'
+        states[2].func = 'Function3'
+
+        const mapData = createMapData(states)
+        expect(mapData.states.length).toBe(3)
+        expect(mapData.states[0]).toMatchObject(states[0])
+        expect(mapData.states[1]).toMatchObject(states[1])
+        expect(mapData.states[2]).toMatchObject(states[2])
+        expect(mapData.elems).toMatchObject(['Element1', 'Element2', 'Element3'])
+        expect(mapData.subElems).toMatchObject(['SubElement1', 'SubElement2', 'SubElement3'])
+        expect(mapData.funcs).toMatchObject(['Function1', 'Function2', 'Function3'])
+    })
 })
