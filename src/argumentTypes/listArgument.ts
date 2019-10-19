21354f5596d0fc39238ff6c19e0cc157a452149b
aoisupersix <aoisupersix@gmail.com>
2019-10-19 09:43:43 +0900

    [add] method of generate variable length arguments

--- src/argumentTypes/listArgument.ts
+++ src/argumentTypes/listArgument.ts
@@ -1,4 +1,7 @@
+import * as Enumerable from 'linq'
+
 import { ArgumentType } from './argumentType'
+import { IArgument } from '../definition/iArgument'
 
 /**
  * 可変長引数をテスト用に生成する数
@@ -7,7 +10,6 @@ import { ArgumentType } from './argumentType'
 const generateArgumentCount = 5
 
 export class ListArgument extends ArgumentType {
-
     public get type(): string {
         return 'List'
     }
@@ -22,6 +24,41 @@ export class ListArgument extends ArgumentType {
 
     public get csharpTestValue(): string {
         return this.rowTestValue
+    }
+
+    /**
+     * 可変長引数を生成して返します。
+     * @param name 引数名
+     * @param description 引数の説明
+     * @param type 引数の型
+     * @param start 引数の開始番号
+     * @param count 生成する引数の数
+     */
+    private generateRangeArgs(
+        name: string,
+        description: string,
+        type: ArgumentType,
+        start: number,
+        count: number
+    ): IArgument[] {
+        const args = Enumerable.range(start, count).select(
+            counter =>
+                ({
+                    name: `${name}${counter}`,
+                    type: type.type,
+                    desc: description,
+                    opt: true,
+                    counter_first: undefined,
+                    last: false,
+                    test_value_map_grammar: type.bve5TestValue,
+                    test_value_map_grammar_non_quote: type.rowTestValue,
+                    test_value_csharp: type.csharpTestValue,
+                    inner_arguments: null
+                } as IArgument)
+        )
+
+        args.last().last = true
+        return args.toArray()
     }
 }
 
