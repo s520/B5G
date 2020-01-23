10687b1911a6ddb5967f4dba057f8c93d3b30da8
a_tanaka <a_tanaka@timedia.co.jp>
2020-01-23 14:00:55 +0900

    [add] createEmptyArgument function

---
+++ src/tests/helper/empty_argument.ts
@@ -0,0 +1,19 @@
+import { IArgument } from '../../definition/iArgument'
+
+/**
+ * 空の引数を生成して返します。
+ */
+export const createEmptyArgument = (): IArgument => {
+    return {
+        name: '',
+        type: '',
+        desc: '',
+        opt: false,
+        last: null,
+        test_value_map_grammar: '',
+        test_value_map_grammar_non_quote: '',
+        test_value_csharp: '',
+        isList: false,
+        inner_arguments: null
+    } as IArgument
+}
