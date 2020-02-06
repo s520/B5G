9704ee0abbe44b8a9bfc1f2acf3a066bd95c6d8f
a_tanaka <a_tanaka@timedia.co.jp>
2020-02-06 18:25:45 +0900

    [add] generating cs test value function

---
+++ src/handlebars-helpers/generate-test-value.ts
@@ -0,0 +1,17 @@
+import { listArgument } from '../argument-types/list-argument'
+import { allTypes } from '../argument-types/all-types'
+
+/**
+ * C#用テスト値を引数の型名を元に生成します。
+ * @param typeName 引数の型名
+ */
+export const generateCSharpTestValue = (typeName: string): string => {
+    const targetType = allTypes.find(t => t.isType(typeName))
+    if (!targetType) {
+        throw new Error(`'${typeName}' is an undefined argument type.`)
+    }
+    if (targetType === listArgument) {
+        throw new Error('Generating C# test values for List type arguments is not supported.')
+    }
+    return targetType.csharpTestValue
+}
